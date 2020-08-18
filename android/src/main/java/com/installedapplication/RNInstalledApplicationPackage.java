1ef4de6fd574fb061762801f76344fb858e0938f
M Danil Rafiqi <danil.rafiqi@gmail.com>
2020-08-19 00:45:07 +0700

    fix naming variab

--- android/src/main/java/com/installedapplication/RNInstalledApplicationPackage.java
+++ android/src/main/java/com/installedapplication/RNInstalledApplicationPackage.java
@@ -14,7 +14,7 @@ import com.facebook.react.bridge.JavaScriptModule;
 public class RNInstalledApplicationPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNInstalledApplicationPackage(reactContext));
+      return Arrays.<NativeModule>asList(new RNInstalledApplicationModule(reactContext));
     }
 
     // Deprecated from RN 0.47
